43c6ecd5cafc5c9083dc2a5b92c8cd71262d499d
markflerko <markflerko@gmail.com>
2022-06-25 22:11:56 +0400

    feat: ts

---
+++ index.ts
@@ -0,0 +1,19 @@
+// import Jimp from "jimp";
+import { httpServer } from "./src/http_server/index";
+import robot from "robotjs";
+import { WebSocketServer } from "ws";
+
+const HTTP_PORT = 3000;
+
+console.log(`Start static http server on the ${HTTP_PORT} port!`);
+httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  console.log("Connection accepted");
+  ws.on("message", (data) => {
+    robot.moveMouseSmooth(1, 1);
+    console.log(data.toString());
+  });
+});
