57291885e3d75f1ea67ad0d3e64574f53e0540da
markflerko <markflerko@gmail.com>
2022-06-26 20:56:42 +0400

    feat: fix circle

--- src/services/drawCircle.ts
+++ src/services/drawCircle.ts
@@ -2,11 +2,9 @@ import robotjs from "robotjs";
 import { Pos } from "types/pos";
 
 export const drawCircle = (pos: Pos, a1: number) => {
-  robotjs.moveMouse(pos.x + a1, pos.y);
-
   robotjs.mouseToggle("down");
   for (let i = 0; i <= Math.PI * 2; i += 0.01) {
-    robotjs.moveMouseSmooth(pos.x + a1 * Math.cos(i), pos.y + a1 * Math.sin(i));
+    robotjs.moveMouseSmooth(pos.x + a1 * Math.cos(i) - a1, pos.y + a1 * Math.sin(i));
   }
   robotjs.mouseToggle("up");
   robotjs.moveMouse(pos.x, pos.y);
