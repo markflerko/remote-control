73525d069b0a718e9a82052cf28a0af3ae5f01fc
markflerko <markflerko@gmail.com>
2022-06-26 22:16:33 +0400

    feat: print screen

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { mouseLeft } from "./services/mouseLeft";
 import { mouseDown } from "./services/mouseDown";
 import { drawRectangle } from "./services/drawRectangle";
 import { drawCircle } from "./services/drawCircle";
+import { printScreen } from "./services/printScreen";
 
 const HTTP_PORT = 3000;
 
@@ -15,10 +16,10 @@ httpServer.listen(HTTP_PORT);
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws) => {
+wss.on("connection", async (ws) => {
   console.log("Connection accepted");
 
-  ws.on("message", (data) => {
+  ws.on("message", async (data) => {
     const [cmd, ...coords] = data.toString().split(" ");
     const a1 = Number(coords[0]) || 0;
     const a2 = Number(coords[1]) || 0;
@@ -55,11 +56,18 @@ wss.on("connection", (ws) => {
       case "draw_circle":
         drawCircle(pos, a1);
         break;
+      case "prnt_scrn":
+        const buffer = await printScreen();
+        ws.send(`prnt_scrn ${buffer.split(";base64,")[1] || ""}`);
+        break;
     }
 
-    if (cmd !== "mouse_position") {
+    if (cmd !== "mouse_position" && cmd !== "prnt_scrn") {
       ws.send(cmd);
     }
+
+    const buffer = await printScreen();
+    ws.send(`prnt_scrn ${buffer.split(";base64,")[1] || ""}`);
 
     console.log(data.toString());
   });
@@ -67,4 +75,16 @@ wss.on("connection", (ws) => {
 
 wss.on("close", () => {
   //Close connection
+});
+
+wss.on("error", (err) => {
+  console.log(err);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(err);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
 });
